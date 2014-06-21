farfanoide/tmuxstart

commit 9d65ffb8f17f44a8162e54b5376733ab452ab1d4

some changes towards ts -n

tmuxstart
@@ -1,64 +1,97 @@
+#!/bin/sh
 #!/usr/bin/env bash
 
-scpt_name=$(basename $0)
-sessions_dir=${TMUXSTART_DIR:-$HOME/.$scpt_name}
+SCPT_NAME=$(basename $0)
+SESSIONS_DIR=${TMUXSTART_DIR:-$HOME/.$SCPT_NAME}
 
 # colors
-reset='\033[0m'; G='\033[0;32m'; Y='\033[0;33m'; R='\033[0;31m'
+RESET='\033[0m'; G='\033[0;32m'; Y='\033[0;33m'; R='\033[0;31m'
 
-if ! type tmux > /dev/null; then echo -e  "${R} tmux not found or not in your PATH${reset}"; fi
+
+if ! type tmux > /dev/null; then echo -e  "${R} tmux not found or not in your PATH${RESET}"; fi
 # local functions
 
 # Print _usage information if there's not enough arguments
 _usage() {
-    echo "${scpt_name} options:"
+    echo "Usage: ${SCPT_NAME} session_name       -- ${Y} Start/Attach session ${RESET}\n"
+    echo "${SCPT_NAME} options:"
     echo "=================="
-    echo -e "${scpt_name} tmux-session    -- ${Y} Start/Attach session ${reset}"
-    echo -e "${scpt_name} -h|--help|help  -- ${Y} Print help menu ${reset}"
-    echo -e "${scpt_name} -l|--list|ls    -- ${Y} List all available session files ${reset}"
-    echo -e "${scpt_name} -o session_name -- ${Y} Edit/Create session file ${reset}"
-    echo -e "${scpt_name} -c session_name -- ${Y} Copy session file to global directory ${reset}"
-    echo -e "${scpt_name} -d session_name -- ${Y} Delete session file ${reset}"
-    echo -e "${scpt_name} -v|--version    -- ${Y} Print ${scpt_name} version number ${reset}"
+    echo " -h|--help|help                           -- ${Y} Print help menu ${RESET}"
+    echo " --list|ls                                -- ${Y} List all available session files ${RESET}"
+    echo " -o session_name [-l]                     -- ${Y} Edit [local] session file ${RESET}"
+    echo " -n session_name [-l] [-e example_file]   -- ${Y} Edit [local] session file ${RESET}"
+    echo " -c session_name                          -- ${Y} Copy session file to global directory ${RESET}"
+    echo " -d session_name                          -- ${Y} Delete session file ${RESET}"
+    echo " -v|--version                             -- ${Y} Print ${SCPT_NAME} version number ${RESET}"
 }
 
 [ $# -lt 1 ] && _usage && exit 1
 
 _print_version() {
     local version
-    version='[ version number here ]'
-    echo "${scpt_name} ${version}"
+    version='[ 0.4.0-alpha ]'
+    echo "${SCPT_NAME} ${G}${version}${RESET}"
+}
+
+_echo_example_file() {
+    local basic_example
+basic_example="
+# General session example
+new_session -n 'General'
+
+# windows:
+general=1; downloads=2;
+
+send_line 'General' 'ls'
+
+new_window -n 'Downloads'
+send_line 'Downloads' 'cd ~/Downloads && ls'
+"
+    if [ -f "$TMUXSTART_DIR/examples/$1" ]; then
+        cat "$TMUXSTART_DIR/examples/$1"
+    else
+        echo $basic_example
+    fi
+}
+_new_session_file() {
+    local session_file session_example dir
+    if [ -z $LOCAL_SESSION ]; then dir=$SESSIONS_DIR; else dir=$PWD; fi
+    session_file="${dir}/${NEW_SESSION_FILE}"
+
+    echo hasta aca llegamos
+    [ -z "$EXAMPLE_FILE" ] && echo $EXAMPLE_FILE
+    [ ! -z $EXAMPLE_FILE ] && session_example=$(_echo_example_file $EXAMPLE_FILE)
+    # EDITOR not explicitly set or uses vim
+    # create a buffer with dummy content in $TMUXSTART_DIR
+    if [[ -z $EDITOR || $EDITOR =~ 'vi' ]] ; then
+        echo "${session_example}" | ${EDITOR:-vi} - "+file ${session_file}"
+    else
+        echo "${session_example}" > $session_file && $EDITOR $session_file
+    fi
+
 }
 
 _open_session_file() {
-    local session_file session_example
-    # TODO: add option to select example file
-session_example="
-# Make an htop window make a regular shell window\n
-new_session -n htop htop\n
-new_window\n
-"
-    session_file="${sessions_dir}/${OPTARG}"
-    if [ ! -f $session_file ]; then
-        # EDITOR not explicitly set or uses vim
-        # create a buffer with dummy content in $TMUXSTART_DIR
-        if [[ -z $EDITOR || $EDITOR =~ 'vi' ]] ; then
-            echo -e "${session_example}" | ${EDITOR:-vi} - +"file ${session_file}"
-        else
-            echo -e "${session_example}" > $session_file;
-            $EDITOR $session_file
-        fi
-    else
+    local session_file dir
+    echo $LOCAL_SESSION
+
+
+    if [ -z $LOCAL_SESSION ]; then dir=$SESSIONS_DIR; else dir=$PWD; fi
+    session_file="${dir}/${OPTARG}"
+
+    if [ -f $session_file ]; then
         ${EDITOR:-vi} $session_file
+    else
+        echo "${R}Session file not found.${RESET}"
     fi
 }
 
 _delete_session_file() {
     local session_file confirm
 
-    session_file="${sessions_dir}/${OPTARG}"
+    session_file="${SESSIONS_DIR}/${OPTARG}"
     if [ -f $session_file ]; then
-        echo -e "${R}Are you sure you want to remove \"${OPTARG}\"? [y/N]${reset}"
+        echo -e "${R}Are you sure you want to remove \"${OPTARG}\"? [y/N]${RESET}"
         read confirm
         [[ $confirm =~ 'y' ]] && rm -v $session_file && exit 0
     else
@@ -68,8 +101,8 @@ _delete_session_file() {
 }
 
 _link_session_file() {
-    if [ -f "./$OPTARG" -a ! -f "${sessions_dir}/${OPTARG}" ]; then
-       ln -s "$PWD/$OPTARG" $sessions_dir && exit 0;
+    if [ -f "./$OPTARG" -a ! -f "${SESSIONS_DIR}/${OPTARG}" ]; then
+        ln -s "$PWD/$OPTARG" $SESSIONS_DIR && exit 0;
     fi
     exit 1
 }
@@ -77,28 +110,28 @@ _link_session_file() {
 _list_session_files() {
     local sessions session session_files
 
-    sessions=($(tmux list-sessions -F '#S' 2> /dev/null))
-    session_files=($(\ls $sessions_dir))
+    sessions=($(tmux list-sessions -F '#S' 2> /dev/null))   # running sessions
+    session_files=($(\ls $SESSIONS_DIR))                    # tmuxstart sessions
 
     if [ ${#session_files[*]} -ge 1 ]; then
-
-        echo -e "${Y}$scpt_name sessions:${reset}"
+        echo "${Y}${SCPT_NAME} sessions:${RESET}"
         for session in ${session_files[@]}; do
+            # remove session_file from
             sessions=(${sessions[@]//*$session*})
             if tmux has-session -t $session 2> /dev/null ; then
-                session="${session} ${G}(running)${reset}"
+                session="${session} ${G}(running)${RESET}"
             fi
-            if [ -h "$sessions_dir/$session" ] ; then
+            if [ -h "$SESSIONS_DIR/$session" ] ; then
                 session="${session} (local)"
             fi
-            echo -e $session
+            echo $session
         done
     else
-        echo -e "${R}No session files found."
+        echo "${R}No session files found."
     fi
 
     if [ ${#sessions[*]} -ge 1 ]; then
-        echo -e "\n${Y}Other running sessions:${reset}"
+        echo "\n${Y}Other running sessions:${RESET}"
         for session in ${sessions[*]}; do
             echo $session
         done
@@ -108,9 +141,10 @@ _list_session_files() {
 # long flags with no hyphens
 [ $1 == 'list' -o $1 == 'ls' ] && _list_session_files && exit 0 ;
 [ $1 == 'help' ] && _usage && exit 0 ;
+[ $1 == 'version' ] && _print_version && exit 0 ;
 
 # arguments parsing loop
-while getopts ':hlvc:o:d:-' option; do
+while getopts ':hle:n:vc:o:d:-' option; do
     case "${option}" in
         h)
             _usage ; exit 0 ;;
@@ -118,8 +152,12 @@ while getopts ':hlvc:o:d:-' option; do
             _link_session_file ;;
         d)
             _delete_session_file ;;
+        e)
+            EXAMPLE_FILE=$OPTARG ;;
+        n)
+            NEW_SESSION_FILE=$OPTARG ;;
         l)
-            _list_session_files ; exit 0 ;;
+            LOCAL_SESSION=1 ;;
         o)
             _open_session_file ; exit 0 ;;
         v)
@@ -141,6 +179,8 @@ while getopts ':hlvc:o:d:-' option; do
             _usage ; exit 1 ;;
     esac
 done
+
+[ ! -z $NEW_SESSION_FILE ] && _new_session_file && exit 0
 
 # Some helper functions for simpler session files
 new_session() {
@@ -169,8 +209,8 @@ if ! tmux has-session -t $session 2> /dev/null ; then
     if [ -f "./$session" ]; then
         . "./$session"
     else
-        if [ -f "$sessions_dir/$session" ] ; then
-            cd $sessions_dir
+        if [ -f "$SESSIONS_DIR/$session" ] ; then
+            cd $SESSIONS_DIR
             [ -h "./$session" ] && cd $(dirname $(readlink "$session"))
             . "./$session"
         else

commit c7625dc50862ce21e06780f17998572847455e65

fixes regressions introduced in 9d65ffb8f17
implements -n option
divides folders in tmuxstart_dir to have globals, locals and examples.
some funcion refactoring
adds variables to easily reference new directory structure

completions/_tmuxstart.zsh
@@ -12,16 +12,20 @@
 #  * Farfanoide (https://github.com/farfanoide)
 #
 # ------------------------------------------------------------------------------
-local sessions_dir running_sessions session_files
+local sessions_dir running_sessions global_sessions
 
 sessions_dir=${TMUXSTART_DIR:-$HOME/.tmuxstart}
 
-session_files=($(\ls $sessions_dir))
-# all_sessions=$session_files
+global_sessions=($(\ls "$sessions_dir/globals"))
+local_sessions=($(\ls "$sessions_dir/locals"))
+# all_sessions=$global_sessions
 
 running_sessions=($(tmux list-sessions -F '#S' 2> /dev/null))
 
-for session in ${session_files[@]}; do
+for session in ${global_sessions[@]}; do
+    running_sessions=(${running_sessions[@]//*$session*})
+done
+for session in ${local_sessions[@]}; do
     running_sessions=(${running_sessions[@]//*$session*})
 done
 
@@ -31,9 +35,9 @@ _arguments -s \
     '-l::List all available session files'\
     '--list::List all available session files'\
     '-c:Copy local session file to global directory:_files ./*'\
-    '-o:Edit/Create session file:($session_files)'\
-    '-d:Delete session file:($session_files)'\
+    '-o:Edit/Create session file:($global_sessions $local_sessions)'\
+    '-d:Delete session file:($global_sessions $local_sessions)'\
     '-v:Print version number'\
     '--version:Print version number'\
-    '*:Start/Attach session:($session_files $running_sessions)'
+    '*:Start/Attach session:($global_sessions $running_sessions $local_sessions)'
 

tmuxstart
@@ -1,157 +1,150 @@
 #!/bin/sh
-#!/usr/bin/env bash
 
 SCPT_NAME=$(basename $0)
 SESSIONS_DIR=${TMUXSTART_DIR:-$HOME/.$SCPT_NAME}
+GLOBALS_DIR="$SESSIONS_DIR/globals"
+LOCALS_DIR="$SESSIONS_DIR/locals"
+EXAMPLES_DIR="$SESSIONS_DIR/examples"
 
 # colors
 RESET='\033[0m'; G='\033[0;32m'; Y='\033[0;33m'; R='\033[0;31m'
 
 
-if ! type tmux > /dev/null; then echo -e  "${R} tmux not found or not in your PATH${RESET}"; fi
+if ! type tmux > /dev/null; then echo -e  "$R tmux not found or not in your PATH $RESET"; fi
 # local functions
 
 # Print _usage information if there's not enough arguments
 _usage() {
-    echo "Usage: ${SCPT_NAME} session_name       -- ${Y} Start/Attach session ${RESET}\n"
-    echo "${SCPT_NAME} options:"
+    echo "Usage: $SCPT_NAME session_name       -- $Y Start/Attach session $RESET\n"
+    echo "$SCPT_NAME options:"
     echo "=================="
-    echo " -h|--help|help                           -- ${Y} Print help menu ${RESET}"
-    echo " --list|ls                                -- ${Y} List all available session files ${RESET}"
-    echo " -o session_name [-l]                     -- ${Y} Edit [local] session file ${RESET}"
-    echo " -n session_name [-l] [-e example_file]   -- ${Y} Edit [local] session file ${RESET}"
-    echo " -c session_name                          -- ${Y} Copy session file to global directory ${RESET}"
-    echo " -d session_name                          -- ${Y} Delete session file ${RESET}"
-    echo " -v|--version                             -- ${Y} Print ${SCPT_NAME} version number ${RESET}"
+    echo " -h | --help | help                       -- $Y Print help menu $RESET"
+    echo " --list | ls                              -- $Y List all available session files $RESET"
+    echo " -o session_name [-l]                     -- $Y Edit [local] session file $RESET"
+    echo " -n session_name [-l] [-e example_file]   -- $Y Create [local] session file [ from example ]$RESET"
+    echo " -c session_name                          -- $Y Copy session file to global directory $RESET"
+    echo " -d session_name                          -- $Y Delete session file $RESET"
+    echo " -v | --version                           -- $Y Print $SCPT_NAME version number $RESET"
 }
 
 [ $# -lt 1 ] && _usage && exit 1
-
-_print_version() {
-    local version
-    version='[ 0.4.0-alpha ]'
-    echo "${SCPT_NAME} ${G}${version}${RESET}"
-}
-
-_echo_example_file() {
-    local basic_example
-basic_example="
-# General session example
-new_session -n 'General'
-
-# windows:
-general=1; downloads=2;
-
-send_line 'General' 'ls'
-
-new_window -n 'Downloads'
-send_line 'Downloads' 'cd ~/Downloads && ls'
-"
-    if [ -f "$TMUXSTART_DIR/examples/$1" ]; then
-        cat "$TMUXSTART_DIR/examples/$1"
-    else
-        echo $basic_example
-    fi
-}
-_new_session_file() {
-    local session_file session_example dir
-    if [ -z $LOCAL_SESSION ]; then dir=$SESSIONS_DIR; else dir=$PWD; fi
-    session_file="${dir}/${NEW_SESSION_FILE}"
-
-    echo hasta aca llegamos
-    [ -z "$EXAMPLE_FILE" ] && echo $EXAMPLE_FILE
-    [ ! -z $EXAMPLE_FILE ] && session_example=$(_echo_example_file $EXAMPLE_FILE)
-    # EDITOR not explicitly set or uses vim
-    # create a buffer with dummy content in $TMUXSTART_DIR
-    if [[ -z $EDITOR || $EDITOR =~ 'vi' ]] ; then
-        echo "${session_example}" | ${EDITOR:-vi} - "+file ${session_file}"
-    else
-        echo "${session_example}" > $session_file && $EDITOR $session_file
-    fi
-
-}
-
-_open_session_file() {
-    local session_file dir
-    echo $LOCAL_SESSION
-
-
-    if [ -z $LOCAL_SESSION ]; then dir=$SESSIONS_DIR; else dir=$PWD; fi
-    session_file="${dir}/${OPTARG}"
-
-    if [ -f $session_file ]; then
-        ${EDITOR:-vi} $session_file
-    else
-        echo "${R}Session file not found.${RESET}"
-    fi
-}
-
-_delete_session_file() {
-    local session_file confirm
-
-    session_file="${SESSIONS_DIR}/${OPTARG}"
-    if [ -f $session_file ]; then
-        echo -e "${R}Are you sure you want to remove \"${OPTARG}\"? [y/N]${RESET}"
-        read confirm
-        [[ $confirm =~ 'y' ]] && rm -v $session_file && exit 0
-    else
-        echo "Session file for \"${OPTARG}\" not found."
-    fi
-    exit 1
-}
-
-_link_session_file() {
-    if [ -f "./$OPTARG" -a ! -f "${SESSIONS_DIR}/${OPTARG}" ]; then
-        ln -s "$PWD/$OPTARG" $SESSIONS_DIR && exit 0;
-    fi
-    exit 1
-}
-
-_list_session_files() {
-    local sessions session session_files
-
-    sessions=($(tmux list-sessions -F '#S' 2> /dev/null))   # running sessions
-    session_files=($(\ls $SESSIONS_DIR))                    # tmuxstart sessions
-
-    if [ ${#session_files[*]} -ge 1 ]; then
-        echo "${Y}${SCPT_NAME} sessions:${RESET}"
-        for session in ${session_files[@]}; do
-            # remove session_file from
-            sessions=(${sessions[@]//*$session*})
-            if tmux has-session -t $session 2> /dev/null ; then
-                session="${session} ${G}(running)${RESET}"
-            fi
-            if [ -h "$SESSIONS_DIR/$session" ] ; then
-                session="${session} (local)"
-            fi
-            echo $session
-        done
-    else
-        echo "${R}No session files found."
-    fi
-
-    if [ ${#sessions[*]} -ge 1 ]; then
-        echo "\n${Y}Other running sessions:${RESET}"
-        for session in ${sessions[*]}; do
-            echo $session
-        done
-    fi
-}
 
 # long flags with no hyphens
 [ $1 == 'list' -o $1 == 'ls' ] && _list_session_files && exit 0 ;
 [ $1 == 'help' ] && _usage && exit 0 ;
 [ $1 == 'version' ] && _print_version && exit 0 ;
 
+_print_version() {
+    local version
+    version='[ 0.4.0-alpha ]'
+    echo "$SCPT_NAME ${G}${version}${RESET}"
+}
+
+_echo_example_file() {
+    if [ -f "$EXAMPLES_DIR/$1" ]; then
+        cat "$EXAMPLES_DIR/$1"
+    else
+        cat "$EXAMPLES_DIR/main"
+    fi
+}
+
+_new_session_file() {
+    local session_file session_example dir
+    if [ -z $LOCAL_SESSION ]; then dir="$SESSIONS_DIR/globals"; else dir=$PWD; fi
+    session_file="$dir/$NEW_SESSION_FILE"
+
+    [ ! -z $EXAMPLE_FILE ] && session_example=$(_echo_example_file $EXAMPLE_FILE)
+    # EDITOR not explicitly set or uses vim
+    # create a buffer with dummy content in $TMUXSTART_DIR
+    if [[ -z $EDITOR || $EDITOR =~ 'vi' ]] ; then
+        echo "$session_example" | ${EDITOR:-vi} - "+file $session_file"
+    else
+        echo "$session_example" > $session_file && $EDITOR $session_file
+    fi
+}
+
+_set_session_file() {
+    local filename
+    filename=$1
+    if [ -f "$LOCALS_DIR/$filename" ]; then
+        echo "$LOCALS_DIR/$filename"
+    else
+        echo "$GLOBALS_DIR/$filename"
+    fi
+}
+_open_session_file() {
+    local session_file dir
+    session_file=$(_set_session_file $1)
+
+    if [ -f $session_file ]; then
+        ${EDITOR:-vi} $session_file
+    else
+        echo "${R}Session file not found.$RESET"
+    fi
+}
+
+_delete_session_file() {
+    local session_file confirm filename
+    filename=$1
+    session_file=$(_set_session_file $filename)
+    if [ -f $session_file ]; then
+        echo "${R}Are you sure you want to remove \"$filename\"? [y/N]$RESET"
+        read confirm
+        [[ $confirm =~ 'y' ]] && rm -v $session_file && exit 0
+    else
+        echo "Session file for \"$filename\" not found."
+    fi
+    exit 1
+}
+
+_link_session_file() {
+    if [ -f "./$OPTARG" -a ! -f "$SESSIONS_DIR/locals/$OPTARG" ]; then
+        ln -s "$PWD/$OPTARG" "$SESSIONS_DIR/locals" && exit 0;
+    fi
+    exit 1
+}
+
+_print_sessions_array() {
+    local sessions_array
+    sessions_array=$*
+    for session in ${sessions_array[*]}; do
+        # remove session_file from running_sessions array
+        sessions=(${sessions[*]//*$session*})
+        if tmux has-session -t "$session" 2> /dev/null ; then session="$session $G(running)$RESET" ; fi
+        echo " - $session"
+    done
+}
+
+_list_session_files() {
+    local sessions session global_sessions local_sessions
+    sessions=($(tmux list-sessions -F '#S' 2> /dev/null))   # running sessions
+    global_sessions=($(\ls "$SESSIONS_DIR/globals"))
+    local_sessions=($(\ls "$SESSIONS_DIR/locals"))
+
+    if [ ${#global_sessions[*]} -ge 1 -o ${#local_sessions[*]} -ge 1 ]; then
+        echo "$Y$SCPT_NAME global sessions:$RESET"
+        _print_sessions_array ${global_sessions[*]}
+        echo "$Y$SCPT_NAME local sessions:$RESET"
+        _print_sessions_array ${local_sessions[*]}
+    else
+        echo "${R}No session files found.$RESET"
+    fi
+
+    if [ ${#sessions[*]} -ge 1 ]; then
+        echo "${Y}Other running sessions:$RESET"
+        for session in ${sessions[*]}; do echo " - $session" ; done
+    fi
+}
+
 # arguments parsing loop
 while getopts ':hle:n:vc:o:d:-' option; do
-    case "${option}" in
+    case "$option" in
         h)
             _usage ; exit 0 ;;
         c)
             _link_session_file ;;
         d)
-            _delete_session_file ;;
+            _delete_session_file $OPTARG ;;
         e)
             EXAMPLE_FILE=$OPTARG ;;
         n)
@@ -159,7 +152,7 @@ while getopts ':hle:n:vc:o:d:-' option; do
         l)
             LOCAL_SESSION=1 ;;
         o)
-            _open_session_file ; exit 0 ;;
+            _open_session_file $OPTARG ; exit 0 ;;
         v)
             _print_version ; exit 0 ;;
         -)
@@ -172,11 +165,9 @@ while getopts ':hle:n:vc:o:d:-' option; do
                     _print_version ; exit 0 ;;
             esac ;;
         :)
-            echo "Option -$OPTARG requires an argument."
-            _usage ; exit 1 ;;
+            echo "Option -$OPTARG requires an argument." ; _usage ; exit 1 ;;
         *)
-            echo "Invalid option"
-            _usage ; exit 1 ;;
+            echo "Invalid option" ; _usage ; exit 1 ;;
     esac
 done
 